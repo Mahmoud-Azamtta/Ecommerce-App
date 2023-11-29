4b2f39ac665c43085afe04f541654e947ddecc33
Mahmoud-Azamtta <134940138+Mahmoud-Azamtta@users.noreply.github.com>
2023-11-29 16:23:57 +0200

    Added tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
       fontFamily: {
         zilla: ["Zilla Slab", "serif"],
       },
+      backgroundImage: {
+        "blob-scene-orange": "url('/images/blob-scene-orange.svg')",
+        "blob-scene-amber": "url('/images/blob-scene-amber.svg')",
+        "blob-scene-light": "url('/images/blob-scene-light.svg')",
+        "home-blob-light": "url('/images/blob-scene-light-home.svg')",
+        "home-blob-dark": "url('/images/blob-scene-dark-home.svg')",
+      },
+      colors: {
+        "light-gray": "#8e9aab",
+      },
     },
   },
   plugins: [],
